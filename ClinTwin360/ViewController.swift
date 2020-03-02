09245af8cd7024189c01fc4e3babe35fffe66eb3
Lauren <lbongartz@me.com>
2020-03-01 21:55:05 -0800

    Created base project

---
+++ ClinTwin360/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClinTwin360
+//
+//  Created by Lauren Bongartz on 3/1/20.
+//  Copyright © 2020 Lauren Bongartz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
